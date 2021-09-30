86bab77e4e6d961ff9c35943de2a7f53344f5b88
Gustavo Seabra <devguga87@gmail.com>
2021-09-30 10:14:06 -0300

    feature:counterprovider

--- src/contexts/PostsProvider/reducer.js
+++ src/contexts/PostsProvider/reducer.js
@@ -2,14 +2,9 @@ import * as types from './types';
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case types.POSTS_LOADING: {
-      console.log(action.type);
-      return { ...state, loading: true };
-    }
     case types.POSTS_SUCCESS: {
-      return { ...state, posts: action.payload, loading: false };
+      return { ...state, posts: action.payload };
     }
   }
-  console.log('nao encontrei a action', action.type);
   return { ...state };
 };
